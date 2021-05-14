49ff59aee63f5006dcddc0f96b5abcbafda6bec4
Fede <fede.2santos_96@hotmail.com>
2021-05-14 13:46:48 -0300

    uploading my practice websites

---
+++ Mi página 6/my script.js
@@ -0,0 +1,20 @@
+/* This script calculates my age so i don't need to remember about changing it every year*/
+
+function calculateAge(date) {
+    var today = new Date();
+    var birthday = new Date(date);
+    var year = today.getFullYear() - birthday.getFullYear();
+    var m = today.getMonth() - birthday.getMonth();
+
+    if (m < 0 || (m === 0 && today.getDate() < birthday.getDate())) {
+        year--;
+    }
+
+    return year;
+}
+
+let birthday = '1996-09-06';
+
+let age = calculateAge(birthday);
+
+document.getElementById("age").innerHTML = age;
